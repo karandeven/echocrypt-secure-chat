a99ff7ba012e03883759a419d772065ec23c73a9
karandeven <karandeven@users.noreply.github.com>
2026-02-08 15:48:54 +0530

    feat: add basic Node.js and Socket.IO backend server

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("message", (data) => {
+    // send message to all connected clients
+    io.emit("message", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+const PORT = 5000;
+server.listen(PORT, () => {
+  console.log(`EchoCrypt server running on port ${PORT}`);
+});
+
